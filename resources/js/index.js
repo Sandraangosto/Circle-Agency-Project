37eeae9d5bcb3bedea5ab9e7b66e5c5debdc6c73
Sandra Angosto <sandra_angosto@icloud.com>
2023-05-31 21:25:01 +0200

    responsive project

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,27 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+
+/*Logica botón menú responsive*/
+const btnmenu = document.getElementsByClassName('btncollapsemenu');
+let isBtnMenuOpen = false;
+btnmenu[0].addEventListener('click', () => {
+
+    console.log(isBtnMenuOpen);
+
+    let elems = document.getElementById('lista-collapse').getElementsByTagName('li');
+    for (let i = 0; i < elems.length; i++) {
+        if(isBtnMenuOpen){
+            elems[i].className="box";
+        }else{
+            elems[i].className="box-show"
+        } 
+        
+    }
+
+    if(!isBtnMenuOpen){
+        btnmenu[0].getElementsByTagName('i')[0].className="fa-solid fa-xmark fa-2xl";
+        isBtnMenuOpen=true;
+    }else{
+        btnmenu[0].getElementsByTagName('i')[0].className="fa-solid fa-bars fa-2xl";
+        isBtnMenuOpen=false;
+    }
+  });
